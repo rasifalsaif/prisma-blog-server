e18e63dc70aa56a8502e839f996916e0529110c9
Rasif Al Saif <rasifalsaif@gmail.com>
2026-02-07 02:38:03 +0600

    first commit

---
+++ src/modules/post/post.router.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { postController } from './post.controller';
+
+const router = express.Router();
+
+router.post('/', postController.createPost);
+
+
+
+export const postRouter = router;
